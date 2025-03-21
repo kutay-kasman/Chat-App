92a942ef491399b16b6611fc3a513c915260c45e
Kutay Kasman <kasman21@itu.edu.tr>
2025-03-21 21:02:24 +0300

    add css and location sender

--- public/js/chat.js
+++ public/js/chat.js
@@ -1,30 +1,121 @@
 const socket = io()
 
-// socket.on("message", (welcome) => {
-//     console.log(welcome)
-// }) 
+// elements
+const messageForm = document.getElementById('messageForm')
+const messageInput = document.getElementById('message')
+const submitButton = document.getElementById('submit-button')
+const sendLocationButton = document.getElementById('send-location')
+const mesContainer = document.getElementById('message-container')
 
-// document.querySelector('#messageForm').addEventListener('submit', (e) => {
-//     e.preventDefault()
-    
-//     const message = e.target.elements.message.value
+//templates
+const mesTemplate = document.getElementById('message-template').innerHTML
+const locationTemplate = document.getElementById('location-template').innerHTML
 
-//     socket.emit('sendMessage', message)
-// })
+socket.on('locationMessage', (locationURL) => {
+    console.log(locationURL)
+
+    const html = Mustache.render(locationTemplate, {
+        locationURL
+    })
+    const tempDiv = document.createElement('div')
+    tempDiv.innerHTML = html
+
+    mesContainer.insertAdjacentElement('beforeend', tempDiv.firstElementChild)
+})
+
+
+socket.on("message", (message) => {
+    console.log(message)
+
+    const html = Mustache.render(mesTemplate , {
+        mes: message
+    })
+    const tempDiv = document.createElement('div')
+    tempDiv.innerHTML = html
+    mesContainer.insertAdjacentElement('beforeend', tempDiv.firstElementChild)
+}) 
+
+messageForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    submitButton.setAttribute('disabled', 'disabled')    // disable sendLocation button while its sending
+
+    const message = e.target.elements.message.value
+
+    socket.emit('sendMessage', message, (err) => {
+
+        submitButton.removeAttribute('disabled')
+        messageInput.value = ""
+        messageInput.focus()
+        
+        if(err) {
+            return console.log(err)
+        }
+
+        console.log('Message is delivered')
+    })
+})
+
+sendLocationButton.addEventListener('click', () => {
+    if(!navigator.geolocation) {
+        return alert('Geolocation is not supported with your browser')
+    }
+    sendLocationButton.setAttribute('disabled', '') // disable sending location before its done. No value need for disabled
+
+    navigator.geolocation.getCurrentPosition((position) => {
+        sendLocationButton.removeAttribute('disabled')
+
+        socket.emit('sendLocation', {
+            latitude: position.coords.latitude,
+            longitude: position.coords.longitude
+        }, (err) => {
+            if(err) {
+                return console.log(err)
+            }
+            console.log('Location is delivered')
+        })
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
 //receive the event sending
 // on is for event listener
-socket.on("countUpdated", (count, userId) => {   // name should be matched exatcly with server sending socket.emit
-    userId++
+// socket.on("countUpdated", (count, userId) => {   // name should be matched exatcly with server sending socket.emit
+//     userId++
     // console.log("The count is ", count, userId)
 
     // const numberShow = document.getElementById('number')
     // numberShow.innerHTML = count
-})    
+// })    
 
-const incrementButton = document.querySelector("#increment")
+// const incrementButton = document.querySelector("#increment")
 // incrementButton.addEventListener('click', () => {
 //     console.log("Clicked")
 //     socket.emit('increment')
@@ -33,28 +124,28 @@ const incrementButton = document.querySelector("#increment")
 
 
 
-const button = document.getElementById('setText')
-const textarea = document.getElementById('mes')
-button.addEventListener('click', () => {
-    const userText = textarea.value
-    console.log('Message was read')
-    socket.emit("sendText", userText)
-    document.getElementById('mes').value = ""
-})
+// const button = document.getElementById('setText')
+// const textarea = document.getElementById('mes')
+// button.addEventListener('click', () => {
+//     const userText = textarea.value
+//     console.log('Message was read')
+//     socket.emit("sendText", userText)
+//     document.getElementById('mes').value = ""
+// })
 
 
-socket.on("showMessage", (userText, userID) => {
-    const message = document.getElementById('message')
-    const newMessageDiv = document.createElement("div")
+// socket.on("showMessage", (userText, userID) => {
+//     const message = document.getElementById('message')
+//     const newMessageDiv = document.createElement("div")
     
-    newMessageDiv.innerText = userText
-    message.appendChild(newMessageDiv)
+//     newMessageDiv.innerText = userText
+//     message.appendChild(newMessageDiv)
 
-    if(userID % 2) {
-        newMessageDiv.style.backgroundColor = "cyan"
-    }
-    else {
-        newMessageDiv.style.backgroundColor = "red"
-    }
+//     if(userID % 2) {
+//         newMessageDiv.style.backgroundColor = "cyan"
+//     }
+//     else {
+//         newMessageDiv.style.backgroundColor = "red"
+//     }
 
-})
+// })
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ const path = require("path")
 const http = require("http")
 const express = require("express")
 const socketio = require("socket.io")
+const Filter = require("bad-words")
 
 
 const app = express()
@@ -14,42 +15,54 @@ const publicDirPath = path.join(__dirname, "../public")
 
 app.use(express.static(publicDirPath))
  
-let count = 0
-let userID = 0
-io.on("connection", (socket) => {  
-    console.log("New web socket connection")
-    userID++
-    socket.emit("countUpdated", count, userID);
+// // let count = 0
+// // let userID = 0
+// io.on("connection", (socket) => {  
+//     // console.log("New web socket connection")
+//     // userID++
+//     // socket.emit("countUpdated", count, userID);
     
 
-    // socket.on('increment', () => {
-    //     count++
-    //     io.emit('countUpdated', count, userID)
-    // })
+//     // socket.on('increment', () => {
+//     //     count++
+//     //     io.emit('countUpdated', count, userID)
+//     // })
 
-    socket.on('sendText', (userText) => {
-        io.emit("showMessage", userText, userID)
-        // console.log(userText)
-    })
-})
+//     socket.on('sendText', (userText) => {
+//         io.emit("showMessage", userText, userID)
+//         // console.log(userText)
+//     })
+// })
 
 io.on("connection", (socket) => {
     console.log("New web socket connection")
 
     socket.emit("message", 'Welcome!!')
-    socket.broadcast.emit("message", "New user joined")
+    //socket.broadcast.emit("message", `${socket.id} joined`)
 
 
-    socket.on('sendMessage', (message) => {
+    socket.on('sendMessage', (message ,callback) => {
+        
+        const filter = new Filter()
+        if(filter.isProfane(message)) {
+            return callback('The profanity is not allowed')
+        }
         io.emit("message", message)
-    })
-
-    socket.on("disconnect", ()=> {
-        socket.broadcast.emit("message", "A user has left")
+        callback()
     
     })
-})
 
+    socket.on('sendLocation', (location, callback) => {
+        const locationURL = `https://www.google.com/maps?q=${location.latitude},${location.longitude}`
+        io.emit('locationMessage', locationURL)
+        callback()
+    })
+
+
+    socket.on("disconnect", ()=> {
+        socket.broadcast.emit("message", `${socket.id} has left`)
+    })
+})
 
 server.listen(port, () => {
     console.log("Server is up on " + port)
