884df6a5f5d6fc38c489626b1d9822f23adb818b
Kutay Kasman <kasman21@itu.edu.tr>
2025-03-27 15:56:00 +0300

    home-page-styling

--- public/js/home-page.js
+++ public/js/home-page.js
@@ -5,15 +5,15 @@ async function fetchRooms() {
         const roomsList = document.getElementById("rooms-list-ul");
         
         // Create the header
-        const header = `<li><strong>Room Name</strong> | <strong>Number of Users</strong></li>`;
+        const header = `<li><strong>Room</strong> | <strong>Users</strong></li>`;
         
         // Check if rooms exist and map over them
         roomsList.innerHTML = rooms.length 
             ? header + rooms.map((room) => {
                 // Ensure both roomName and userNum are available
                 return room.roomName && room.userNum ? 
-                    `<li>${room.roomName}&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;
-                    |&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;${room.userNum}</li>` : '';
+                    `<li id="home-page-room-list"><div id="room">${room.roomName}</div><div id="users">${room.userNum}</li></div>
+                    ` : '';
             }).join('') 
             : "<li>No active rooms</li>";
     } catch (error) {
